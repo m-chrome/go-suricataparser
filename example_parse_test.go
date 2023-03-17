fb584168143a853726f7c1c1ebbbb608b61bc41c
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2023-03-18 00:17:09 +0300

    Add parse rule example

---
+++ example_parse_test.go
@@ -0,0 +1,35 @@
+package suricataparser_test
+
+import (
+	"fmt"
+
+	"github.com/m-chrome/go-suricataparser"
+)
+
+func ExampleParseRule() {
+	rule, _ := suricataparser.ParseRule(
+		"alert http any any -> [1.1.1.1] any (sid:1; rev:1; gid:1; http_uri; msg:\"message\";)")
+	fmt.Println(rule)
+	fmt.Println(rule.Enabled)
+	fmt.Println(rule.Sid())
+	fmt.Println(rule.Rev())
+	fmt.Println(rule.Msg())
+	fmt.Println(rule.Action())
+	fmt.Println(rule.Header())
+	opts := rule.GetOptions("http_uri")
+	fmt.Println(opts[0])
+	rule.Enabled = false
+	fmt.Println(rule.Enabled)
+	fmt.Println(rule)
+	// Output:
+	// alert http any any -> [1.1.1.1] any (sid:1; rev:1; gid:1; http_uri; msg:"message";)
+	// true
+	// 1
+	// 1
+	// message
+	// alert
+	// http any any -> [1.1.1.1] any
+	// http_uri;
+	// false
+	// # alert http any any -> [1.1.1.1] any (sid:1; rev:1; gid:1; http_uri; msg:"message";)
+}
